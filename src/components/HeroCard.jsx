a7056da890b6ca2e4f98a9b6f8baff8b30b4a5c1
iam-sphinx <kandarp1803@gmail.com>
2023-06-17 12:48:45 +0530

    testing

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -4,16 +4,13 @@ import Marquee from "react-fast-marquee";
 const HeroCard = () => {
   return (
     <div className="relative flex flex-col justify-center items-center">
-      <div className="absolute bottom-[6.75rem]">
+      <div className="absolute bottom-[6.75rem] w-full">
         <Marquee>
-          <img src="icons/infiniteScrollMain.svg" className="w-screen" />
+          <img src="icons/infiniteScrollMain.svg" className="w-[100vw]" />
         </Marquee>
       </div>
 
-      <img
-        src="icons/heroCard.svg"
-        className=" relative z-10"
-      />
+      <img src="icons/heroCard.svg" className=" relative z-10" />
     </div>
   );
 };

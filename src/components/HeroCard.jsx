48e888a57e7d956287179aa1113e064a80aa2b7d
iam-sphinx <kandarp1803@gmail.com>
2023-06-21 18:53:09 +0530

    minor changes

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -8,17 +8,23 @@ const HeroCard = () => {
       <div className="relative flex flex-col justify-center items-center">
         <div className="absolute bottom-[6.75rem] w-full hidden sm:block">
           <Marquee direction="right">
-            <img src="icons/scroll1.png" className="w-[100vw]"  />
+            <img
+              src="icons/scroll1.png"
+              className="w-[94.75rem] h-[11.8125rem]"
+            />
           </Marquee>
         </div>
 
         <div className="sm:hidden block absolute bottom-[5.75rem] w-full">
-          <Marquee>
-            <img src="icons/infiniteScrollMAIN.png" className="w-[100vw]" />
+          <Marquee direction="right">
+            <img
+              src="icons/infiniteScrollMAIN.png"
+              className="w-[24.3625rem] h-[5.875rem]"
+            />
           </Marquee>
         </div>
         <div className=" relative z-10 sm:w-[24.375rem] sm:h-[42.375rem] w-[15.7825rem] h-[27.4375rem]">
-          <img src="icons/heroCard1.png"  />
+          <img src="icons/heroCard1.png" />
         </div>
       </div>
     </CustomAnimation>

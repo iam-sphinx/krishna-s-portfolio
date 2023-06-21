7231f937fb5230547f867988c6459e5fdf64f66e
iam-sphinx <kandarp1803@gmail.com>
2023-06-21 18:19:12 +0530

    testing

--- src/pages/SidebarPage.jsx
+++ src/pages/SidebarPage.jsx
@@ -19,53 +19,126 @@ const SidebarPage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-[#00030a] relative">
-      <Link to={route}>
-        <div className="absolute sm:top-[6.6875rem] top-[6.6875rem] sm:right-[5.75rem] right-[1.6875rem] sm:h-auto sm:w-auto h-[1rem] w-[1rem]">
-          <img src="icons/Vector.svg"  />
+    <div className="min-h-screen bg-[#00030a] flex justify-center">
+      <div className="sm:w-[94.5rem] w-[24.375rem] flex flex-col relative">
+        <div className="flex justify-between items-center mt-[6.6875rem] sm:px-[5.75rem] px-[1.6875rem] sm:mb-[5.0625rem] mb-[3.875rem]">
+          <Link to="/home">
+            <div className="w-[37px] h-[37px] md:w-[47px] md:h-[47px]">
+              <img
+                src="icons/profile.svg"
+                alt=""
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </Link>
+
+          <Link to={route}>
+            <div className="sm:h-auto sm:w-auto h-[1rem] w-[1rem]">
+              <img src="icons/Vector.svg" />
+            </div>
+          </Link>
         </div>
-      </Link>
 
-      <div className="sm:w-[74.5rem] sm:h-[29.0625rem] h-[9.9375rem] w-[22.6875rem] flex flex-wrap sm:pl-12 pl-5 justify-end absolute sm:top-[12.75rem] sm:left-[12.75rem] top-[11.5625rem] right-[1.6875rem]">
-        <Link to="/home">
-          <h1
-            className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[2.75rem] leading-[3.3rem] tracking-[0.25rem] text-right ${
-              route === "/home" ? "text-white" : "text-[#2D2D2D]"
-            }`}
-          >
-            Home \
-          </h1>
-        </Link>
+        <div className="flex justify-end sm:px-[7.25rem] px-[1.6875rem]">
+          <div className="sm:w-[74.5rem] sm:h-[29.0625rem] h-[9.9375rem] w-[22.6875rem] flex flex-wrap sm:pl-12 pl-5 justify-end ">
+            <Link to="/home">
+              <h1
+                className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
+                  route === "/home" ? "text-white" : "text-[#2D2D2D]"
+                }`}
+              >
+                Home \
+              </h1>
+            </Link>
 
-        <Link to="/work">
-          <h1
-            className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[2.75rem] leading-[3.3rem] tracking-[0.25rem] text-right ${
-              route === "/work" ? "text-white" : "text-[#2D2D2D]"
-            }`}
-          >
-            Work \
-          </h1>
-        </Link>
-        <Link to="/about_me">
-          <h1
-            className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[2.75rem] leading-[3.3rem] tracking-[0.25rem] text-right ${
-              route === "/about_me" ? "text-white" : "text-[#2D2D2D]"
-            }`}
-          >
-            About \
-          </h1>
-        </Link>
-        <Link to="/contact">
-          <h1
-            className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[2.75rem] leading-[3.3rem] tracking-[0.25rem] text-right  ${
-              route === "/contact" ? "text-white" : "text-[#2D2D2D]"
-            }`}
-          >
-            Contact \
-          </h1>
-        </Link>
+            <Link to="/work">
+              <h1
+                className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
+                  route === "/work" ? "text-white" : "text-[#2D2D2D]"
+                }`}
+              >
+                Work \
+              </h1>
+            </Link>
+            <Link to="/about_me">
+              <h1
+                className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
+                  route === "/about_me" ? "text-white" : "text-[#2D2D2D]"
+                }`}
+              >
+                About \
+              </h1>
+            </Link>
+            <Link to="/contact">
+              <h1
+                className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right  ${
+                  route === "/contact" ? "text-white" : "text-[#2D2D2D]"
+                }`}
+              >
+                Contact \
+              </h1>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
+    // <div className="min-h-screen bg-[#00030a] relative">
+    //   <Link to={route}>
+    //     <div className="absolute sm:top-[6.6875rem] top-[6.6875rem] sm:right-[5.75rem] right-[1.6875rem] sm:h-auto sm:w-auto h-[1rem] w-[1rem]">
+    //       <img src="icons/Vector.svg" />
+    //     </div>
+    //   </Link>
+
+    //   <Link to={"/home"}>
+    //     <div className="w-[34.875rem] h-[34.875rem] hidden sm:block absolute top-[6.6875rem] left-[5.75rem]">
+    //       <img src="icons/Logo.png" />
+    //     </div>
+
+    //     <div className="w-[2.3125rem] h-[2.3125rem] block sm:hidden absolute top-[6.6875rem] left-[1.6875rem]">
+    //       <img src="icons/MobileLogo.png" />
+    //     </div>
+    //   </Link>
+
+    //   <div className="sm:w-[74.5rem] sm:h-[29.0625rem] h-[9.9375rem] w-[22.6875rem] flex flex-wrap sm:pl-12 pl-5 justify-end absolute sm:top-[12.75rem] sm:left-[12.75rem] top-[11.5625rem] right-[1.6875rem]">
+    //     <Link to="/home">
+    //       <h1
+    //         className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
+    //           route === "/home" ? "text-white" : "text-[#2D2D2D]"
+    //         }`}
+    //       >
+    //         Home \
+    //       </h1>
+    //     </Link>
+
+    //     <Link to="/work">
+    //       <h1
+    //         className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
+    //           route === "/work" ? "text-white" : "text-[#2D2D2D]"
+    //         }`}
+    //       >
+    //         Work \
+    //       </h1>
+    //     </Link>
+    //     <Link to="/about_me">
+    //       <h1
+    //         className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
+    //           route === "/about_me" ? "text-white" : "text-[#2D2D2D]"
+    //         }`}
+    //       >
+    //         About \
+    //       </h1>
+    //     </Link>
+    //     <Link to="/contact">
+    //       <h1
+    //         className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right  ${
+    //           route === "/contact" ? "text-white" : "text-[#2D2D2D]"
+    //         }`}
+    //       >
+    //         Contact \
+    //       </h1>
+    //     </Link>
+    //   </div>
+    // </div>
   );
 };
 
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -49,27 +49,29 @@ const Work = () => {
     },
   ];
   return (
-    <div className="min-h-screen overflow-x-hidden bg-[#00030a] flex flex-col">
-      {/* <Header /> */}
-      <div className="flex-[1] sm:px-[7.25rem] px-6">
-        <Navbar />
+    <div className="min-h-screen overflow-x-hidden bg-[#00030a] flex justify-center">
+      <div className="sm:w-[94.5rem] w-[24.375rem] flex flex-col relative">
+        {/* <Header /> */}
+        <div className="flex-[1] sm:px-[7.25rem] px-6">
+          <Navbar />
 
-        <div className="sm:mt-[7.563rem] mt-16 sm:mb-[6.625rem] mb-[5.25rem]">
-          <CustomAnimation>
-            <h1 className="font-Eskell_Display font-light sm:text-[5.75rem] sm:leading-[6.875rem] text-[2.125rem] leading-[2.55rem] tracking-[0.063rem] sm:mb-[4.5rem] mb-[1.125rem] text-white max-w-[78.5rem]">
-              Somewhere between staring at the screen and tilting my head the
-              magic is done.
-            </h1>
-            <p className="font-GT_America_Trial sm:text-2xl text-base text-justify text-[#86939F] sm:max-w-[54rem] max-w-[21.375rem]">
-              I specialise in crafting interfaces, products, and systems through
-              close collaboration with engineers, researchers, product managers,
-              and fellow designers. From concept to high-fidelity prototypes, I
-              create intuitive and polished experiences that captivate users.
-            </p>
-          </CustomAnimation>
-        </div>
-        {/* Projects */}
-      
+          <div className="sm:mt-[7.563rem] mt-16 sm:mb-[6.625rem] mb-[5.25rem]">
+            <CustomAnimation>
+              <h1 className="font-Eskell_Display font-light sm:text-[5.75rem] sm:leading-[6.875rem] text-[2.125rem] leading-[2.55rem] tracking-[0.063rem] sm:mb-[4.5rem] mb-[1.125rem] text-white max-w-[78.5rem]">
+                Somewhere between staring at the screen and tilting my head the
+                magic is done.
+              </h1>
+              <p className="font-GT_America_Trial sm:text-2xl text-base text-justify text-[#86939F] sm:max-w-[54rem] max-w-[21.375rem]">
+                I specialise in crafting interfaces, products, and systems
+                through close collaboration with engineers, researchers, product
+                managers, and fellow designers. From concept to high-fidelity
+                prototypes, I create intuitive and polished experiences that
+                captivate users.
+              </p>
+            </CustomAnimation>
+          </div>
+          {/* Projects */}
+
           {workSamples.map((item, index) => {
             return (
               <div key={index}>
@@ -77,9 +79,10 @@ const Work = () => {
               </div>
             );
           })}
-        
-        <div className="mt-[7.186rem]">
-          <Footer />
+
+          <div className="mt-[7.186rem]">
+            <Footer />
+          </div>
         </div>
       </div>
     </div>

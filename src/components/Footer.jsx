44178e0f73db35e750cfd4c336b74a7de27a961c
iam-sphinx <kandarp1803@gmail.com>
2023-06-23 20:38:18 +0530

    completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,6 +11,7 @@ const Footer = () => {
     } else {
       navigate(route);
     }
+    window.scrollTo(0, 0);
   };
 
   const handleClick = (site) => {
@@ -49,90 +50,87 @@ const Footer = () => {
         />
       </div>
       <div className="w-full md:flex md:justify-between md:flex-row-reverse mb-6 sm:mb-0">
-        <Fade duration={1200} triggerOnce>
-          <div className="md:flex md:gap-[65px]">
-            <ul className="mb-[22px] ">
-              <li className="font-GT_America_Trial font-normal text-base text-white mb-3">
-                In this space
+        <div className="md:flex md:gap-[65px]">
+          <ul className="mb-[22px] ">
+            <li className="font-GT_America_Trial font-normal text-base text-white mb-3">
+              In this space
+            </li>
+            <li
+              className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-3 cursor-pointer"
+              onClick={() => handleNavigate("/work")}
+            >
+              Discover my work
+            </li>
+            <li
+              className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-3 cursor-pointer"
+              onClick={() => handleNavigate("/about_me")}
+            >
+              Know more about me
+            </li>
+            <li
+              className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-3 cursor-pointer "
+              onClick={() => handleNavigate("/notes")}
+            >
+              Read my notes
+            </li>
+            <li
+              className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-3 cursor-pointer"
+              onClick={() => handleNavigate("/contact")}
+            >
+              Get in touch
+            </li>
+          </ul>
+
+          <div>
+            <ul className="mb-2">
+              <li className="font-GT_America_Trial font-normal text-base text-white mb-4 cursor-pointer">
+                Follow my work
               </li>
               <li
-                className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-3 cursor-pointer"
-                onClick={() => handleNavigate("/work")}
+                className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-4 cursor-pointer"
+                onClick={() => handleClick("Github")}
               >
-                Discover my work
+                Check my Github projects
               </li>
               <li
-                className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-3 cursor-pointer"
-                onClick={() => handleNavigate("/about_me")}
+                className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-4 cursor-pointer"
+                onClick={() => handleClick("Dribble")}
               >
-                Know more about me
-              </li>
-              <li
-                className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-3 cursor-pointer "
-                onClick={() => handleNavigate("/notes")}
-              >
-                Read my notes
-              </li>
-              <li
-                className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-3 cursor-pointer"
-                onClick={() => handleNavigate("/contact")}
-              >
-                Get in touch
+                Check my Dribble shots
               </li>
             </ul>
 
-            <div>
-              <ul className="mb-2">
-                <li className="font-GT_America_Trial font-normal text-base text-white mb-4 cursor-pointer">
-                  Follow my work
-                </li>
-                <li
-                  className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-4 cursor-pointer"
-                  onClick={() => handleClick("Github")}
-                >
-                  Check my Github projects
-                </li>
-                <li
-                  className="font-GT_America_Trial font-normal text-base hover:text-white text-[#88939E] mb-4 cursor-pointer"
-                  onClick={() => handleClick("Dribble")}
-                >
-                  Check my Dribble shots
-                </li>
-              </ul>
+            {/* Socials */}
+            <div className="flex gap-[10px] mb-[66.82px]">
+              {/* instagram */}
+              <div
+                className="h-[36.18px] md:h-[45px] md:w-[45px] w-[36.18px] rounded-full border-[0.36px] border-[#88969E] flex justify-center items-center cursor-pointer"
+                onClick={() => handleClick("instagram")}
+              >
+                <img src="icons/insta.svg" alt="" />
+              </div>
 
-              {/* Socials */}
-              <div className="flex gap-[10px] mb-[66.82px]">
-                {/* instagram */}
-                <div
-                  className="h-[36.18px] md:h-[45px] md:w-[45px] w-[36.18px] rounded-full border-[0.36px] border-[#88969E] flex justify-center items-center cursor-pointer"
-                  onClick={() => handleClick("instagram")}
-                >
-                  <img src="icons/insta.svg" alt="" />
-                </div>
-
-                {/* Linkedin */}
-                <div
-                  className="h-[36.18px] md:h-[45px] md:w-[45px] w-[36.18px] rounded-full border-[0.36px] border-[#88969E] flex justify-center items-center cursor-pointer"
-                  onClick={() => handleClick("linkedIn")}
-                >
-                  <img src="icons/linkedin.svg" alt="" />
-                </div>
+              {/* Linkedin */}
+              <div
+                className="h-[36.18px] md:h-[45px] md:w-[45px] w-[36.18px] rounded-full border-[0.36px] border-[#88969E] flex justify-center items-center cursor-pointer"
+                onClick={() => handleClick("linkedIn")}
+              >
+                <img src="icons/linkedin.svg" alt="" />
               </div>
             </div>
           </div>
-        </Fade>
+        </div>
 
         {/* info & copyright*/}
-        <Fade triggerOnce duration={1200}>
-          <div>
-            <h1 className="font-GT_America_Trial font-normal md:text-2xl text-sm text-white mb-[6px]">
-              Krishna Khanikar
-            </h1>
-            <h1 className="font-normal font-GT_America_Trial text-xs md:text-base text-[#88939E]">
-              All right reserved | {year}{" "}
-            </h1>
-          </div>
-        </Fade>
+
+        <div>
+          <h1 className="font-GT_America_Trial font-normal md:text-2xl text-sm text-white mb-[6px]">
+            Krishna Khanikar
+          </h1>
+          <h1 className="font-normal font-GT_America_Trial text-xs md:text-base text-[#88939E]">
+            All right reserved | {year}{" "}
+          </h1>
+        </div>
       </div>
     </>
   );

7bdc6bb194cfcf934bb54709c3e6daad8006cc4e
iam-sphinx <kandarp1803@gmail.com>
2023-06-23 19:10:00 +0530

    completed

--- src/pages/SidebarPage.jsx
+++ src/pages/SidebarPage.jsx
@@ -7,21 +7,24 @@ const SidebarPage = () => {
   const data = location.state;
 
   let route = "";
-  if (data.url.includes("/home")) {
-    route = "/home";
-  } else if (data.url.includes("/work")) {
+  
+  if (data.url.includes("/work")) {
     route = "/work";
   } else if (data.url.includes("/about_me")) {
     route = "/about_me";
   } else if (data.url.includes("/contact")) {
     route = "/contact";
   }
+  else
+  {
+    route = "/";
+  }
 
   return (
     <div className="min-h-screen bg-[#00030a] flex justify-center">
       <div className="sm:w-[94.5rem] w-[24.375rem] flex flex-col relative">
         <div className="flex justify-between items-center mt-[6.6875rem] sm:px-[5.75rem] px-[1.6875rem] sm:mb-[5.0625rem] mb-[3.875rem]">
-          <Link to="/home">
+          <Link to="/">
             <div className="w-[37px] h-[37px] md:w-[47px] md:h-[47px]">
               <img
                 src="icons/profile.svg"
@@ -40,10 +43,10 @@ const SidebarPage = () => {
 
         <div className="flex justify-end sm:px-[7.25rem] px-[1.6875rem]">
           <div className="sm:w-[74.5rem] sm:h-[29.0625rem] h-[9.9375rem] w-[22.6875rem] flex flex-wrap sm:pl-12 pl-5 justify-end ">
-            <Link to="/home">
+            <Link to="/">
               <h1
                 className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
-                  route === "/home" ? "text-white" : "text-[#2D2D2D]"
+                  route === "/" ? "text-white" : "text-[#2D2D2D]"
                 }`}
               >
                 Home \
@@ -81,63 +84,6 @@ const SidebarPage = () => {
         </div>
       </div>
     </div>
-    // <div className="min-h-screen bg-[#00030a] relative">
-    //   <Link to={route}>
-    //     <div className="absolute sm:top-[6.6875rem] top-[6.6875rem] sm:right-[5.75rem] right-[1.6875rem] sm:h-auto sm:w-auto h-[1rem] w-[1rem]">
-    //       <img src="icons/Vector.svg" />
-    //     </div>
-    //   </Link>
-
-    //   <Link to={"/home"}>
-    //     <div className="w-[34.875rem] h-[34.875rem] hidden sm:block absolute top-[6.6875rem] left-[5.75rem]">
-    //       <img src="icons/Logo.png" />
-    //     </div>
-
-    //     <div className="w-[2.3125rem] h-[2.3125rem] block sm:hidden absolute top-[6.6875rem] left-[1.6875rem]">
-    //       <img src="icons/MobileLogo.png" />
-    //     </div>
-    //   </Link>
-
-    //   <div className="sm:w-[74.5rem] sm:h-[29.0625rem] h-[9.9375rem] w-[22.6875rem] flex flex-wrap sm:pl-12 pl-5 justify-end absolute sm:top-[12.75rem] sm:left-[12.75rem] top-[11.5625rem] right-[1.6875rem]">
-    //     <Link to="/home">
-    //       <h1
-    //         className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
-    //           route === "/home" ? "text-white" : "text-[#2D2D2D]"
-    //         }`}
-    //       >
-    //         Home \
-    //       </h1>
-    //     </Link>
-
-    //     <Link to="/work">
-    //       <h1
-    //         className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
-    //           route === "/work" ? "text-white" : "text-[#2D2D2D]"
-    //         }`}
-    //       >
-    //         Work \
-    //       </h1>
-    //     </Link>
-    //     <Link to="/about_me">
-    //       <h1
-    //         className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right ${
-    //           route === "/about_me" ? "text-white" : "text-[#2D2D2D]"
-    //         }`}
-    //       >
-    //         About \
-    //       </h1>
-    //     </Link>
-    //     <Link to="/contact">
-    //       <h1
-    //         className={`font-Eskell_Display  sm:text-[8.07rem] sm:leading-[9.6837rem] text-[3.75rem] sm:mb-0 mb-4 leading-[3.3rem] tracking-[0.25rem] text-right  ${
-    //           route === "/contact" ? "text-white" : "text-[#2D2D2D]"
-    //         }`}
-    //       >
-    //         Contact \
-    //       </h1>
-    //     </Link>
-    //   </div>
-    // </div>
   );
 };
 
